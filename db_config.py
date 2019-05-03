fefeaf7aa9816df98a91653aec6997721dfbe371
mathieu <mathieu.hautebas@gmail.com>
2019-05-03 21:44:36 +0200

    update

--- db_config.py
+++ db_config.py
@@ -6,6 +6,6 @@ mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'youtube'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'youtube'
-app.config['MYSQL_DATABASE_DB'] = 'mydb'
+app.config['MYSQL_DATABASE_DB'] = 'youtube'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
