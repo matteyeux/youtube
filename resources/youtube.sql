8d04ee63b8c704efb3d809c71a47e2ee321c6dde
mathieu <mathieu.hautebas@gmail.Com>
2019-05-10 17:08:26 +0200

    fix shitty sql issue

--- resources/youtube.sql
+++ resources/youtube.sql
@@ -80,7 +80,7 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `pseudo` varchar(45) DEFAULT NULL,
