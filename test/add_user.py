5798e17fd32a1d1b8b7a0ccc15a2a09f95d22402
mathieu <mathieu.hautebas@gmail.com>
2019-05-11 19:23:06 +0200

    merge to master

--- test/add_user.py
+++ test/add_user.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 # script to add a user
 # to the database
-
+import random
+import string
 import requests
+
+def random_str():
+	letters = string.ascii_lowercase
+	return ''.join(random.sample(letters, 8))
+
 api_endpoint = "http://127.0.0.1:5000/user"
 
+string_to_gen = randomString2()
+mail = string_to_gen + "@pornhub"
 params = {
-		'username' : "weed",
-		'email' : "weed@gmail.com",
+		'username' : string_to_gen,
+		'email' : mail,
 		'pseudo' : "great",
 		'password' : "ccc"
 	}
