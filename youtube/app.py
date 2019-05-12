14c5e6ebc98e29f13cf9721c0cd47fffec074be4
Boris <boris.lepage@gmail.com>
2019-05-12 02:19:42 +0200

    first step to generate token for user

--- youtube/app.py
+++ youtube/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://youtube:youtube@localhost/youtube'
 
 # https://stackoverflow.com/questions/33738467/how-do-i-know-if-i-can-disable-sqlalchemy-track-modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
