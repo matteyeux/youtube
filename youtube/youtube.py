40747062b80174f38ff46d520b2e9cb470d89feb
Boris <boris.lepage@gmail.com>
2019-05-16 13:41:58 +0200

    add 404 global function

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
