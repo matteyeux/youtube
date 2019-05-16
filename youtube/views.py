37652f1e725eea498ef06845dd7ca5517fb991af
Boris <boris.lepage@gmail.com>
2019-05-16 11:37:30 +0200

    add global function to set 404 not found

--- youtube/views.py
+++ youtube/views.py
@@ -1,6 +1,10 @@
 from youtube import app
 from flask import jsonify
 
-@app.route('/')
-def index():
-	return jsonify({'message': 'Welcome on API Youtube'})
+# @app.errorhandler(404)
+# def page_not_found(e):
+# 	return jsonify({'message': 'Not Found'}), 404
+
+# @app.route('/')
+# def index():
+# 	return jsonify({'message': 'Welcome on API Youtube'})
--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
@@ -15,6 +15,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(24)
 
 db = SQLAlchemy(app)
+
+# TODO : Put Index and page_not_found in file 'wiews'
+@app.route('/')
+def index():
+	return 'Welcome on API Youtube'
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return jsonify({'message': 'Not found'}), 404
 
 @app.before_first_request
 def create_tables():
