d2b470f3ab42b76053cf80d94676f33cc0d42fe5
mathieu <mathieu.hautebas@gmail.Com>
2019-05-27 11:19:29 +0200

    partialy working

--- resources/youtube_v2.sql
+++ resources/youtube_v2.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `comment`
 --
 
+DROP DATABASE IF EXISTS youtube;
+CREATE DATABASE IF NOT EXISTS youtube;
+
 DROP TABLE IF EXISTS `comment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
