df662059f88635760e8c9db18470fc19f8d1fe64
mathieu <mathieu.hautebas@gmail.Com>
2019-05-29 17:40:02 +0200

    update code

--- encoder/src/encoder.py
+++ encoder/src/encoder.py
@@ -5,6 +5,8 @@ import ffmpeg
 import sys
 import subprocess
 import os
+import filetype
+from pymediainfo import MediaInfo
 
 resolution_types = [
 	["4K", "3840", "2160"],
@@ -14,6 +16,42 @@ resolution_types = [
 	["360p", "640", "360"],
 	["240p", "426", "240"]
 ]
+
+def is_video(imported_file):
+	media_info = MediaInfo.parse(imported_file)
+	for track in media_info.tracks:
+		if track.track_type == 'Video':
+			return True
+	return False
+
+def is_mp4(video):
+	type_of_file = filetype.guess(video)
+
+	if type_of_file is None:
+		print("Cannot guess filetype")
+		return False
+	
+	print(type_of_file.extension)
+	if type_of_file.extension is "mp4":
+		return True
+	else :
+		return False
+
+def convert_to_mp4(vid_input):
+	FNULL = open(os.devnull, 'w')
+	
+	# should replace extension to mp4
+	vid_output = vid_input
+	vid_output = vid_output.replace(vid_output.split('.')[-1], "mp4")
+	print("convert to mp4")
+	try :
+		p = subprocess.Popen(['ffmpeg', '-i', vid_input, vid_output, '-y'], stdout=FNULL, stderr=FNULL)
+		p.wait()
+	except:
+		print("error")
+		sys.exit(1)
+
+	return vid_output
 
 def do_encode(vid_input, width, height):
 	encoding = width + "x" + height
@@ -25,11 +63,11 @@ def do_encode(vid_input, width, height):
 	# -n stand for : not overwrite output files in non-interactive mode
 	# put -y for overwriting
 	try :
-		p = subprocess.Popen(['ffmpeg', '-i', vid_input, '-s', encoding, vid_output, '-n'], stdout=FNULL, stderr=FNULL)
+		p = subprocess.Popen(['ffmpeg', '-i', vid_input, '-s', encoding, vid_output, '-n']) #, stdout=FNULL, stderr=FNULL)
 		p.wait()
 	except:
-			print("error")
-			sys.exit(1)
+		print("error")
+		sys.exit(1)
 
 def get_video_res(video):
 	try:
@@ -51,7 +89,7 @@ def get_video_res(video):
 def set_resolution(video):
 	width, height = get_video_res(video)
 	video_res = width * height
-
+	print(width, height)
 	for i in range(len(resolution_types)) :
 		resolution = int(resolution_types[i][1]) * int(resolution_types[i][2])
 		if resolution < video_res:
@@ -63,4 +101,18 @@ if __name__ == '__main__':
 		print("usage : %s [video]" % sys.argv[0])
 		sys.exit(1)
 
-	set_resolution(sys.argv[1])
+	video = sys.argv[1]
+	
+	if is_video(video) is False:
+		print("file is not video")
+		sys.exit(1)
+	else :
+		print("is vid gr8")
+
+	if is_mp4(video) is False:
+		print("not mp4")
+		video = convert_to_mp4(video)
+		print(video)
+
+	print("set res")
+	set_resolution(video)
