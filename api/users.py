3a1520bf0c610aff915f0a558e5027e8260eafb0
mathieu <mathieu.hautebas@gmail.Com>
2019-05-29 16:27:41 +0200

    changed API struct

--- api/users.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-# endpoint to show all users
-@app.route("/users", methods=["GET"])
-def users():
-	all_users = User.query.all()
-	result = users_schema.dump(all_users)
-	return jsonify(result.data)
