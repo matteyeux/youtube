9dd263743d0851c9b66124c3ef573fe0bdac9c03
Boris <boris.lepage@gmail.com>
2019-06-04 18:59:22 +0200

    add file install_docker_ubuntu.sh we need to merge with install_docker.sh

--- scripts/install_docker_ubuntu.sh
+++ scripts/install_docker_ubuntu.sh
@@ -16,13 +16,13 @@ sudo apt-get -y -qq install \
 
 
 # downloading Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # check fingerprint
 sudo apt-key fingerprint 0EBFCD88
 
 sudo add-apt-repository \
-	"deb [arch=amd64] https://download.docker.com/linux/debian \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 	$(lsb_release -cs) \
 	stable"
 
