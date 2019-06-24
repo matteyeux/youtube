deaa7a611849f877f1a1c592aba73df0ac653e5f
Boris <boris.lepage@gmail.com>
2019-06-24 15:20:50 +0200

    add new front

---
+++ newFront/myyoutubeapp/pages/getusers.vue
@@ -0,0 +1,30 @@
+<template>
+  <article>
+    <section>
+      <h1>GET API Result List Users</h1>
+      <div>
+        <ul>
+          <li v-for="user in users.data" class="item">
+          	<p>Pseudo : {{ user.pseudo }} (date de création : {{ user.created_at }})</p>
+            <!-- <nuxt-link :to="'/user/' + user.id">{{ user.pseudo }}</nuxt-link> -->
+          </li>
+        </ul>
+      </div>
+    </section>
+  </article>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    async asyncData () {
+      const {data} = await axios.get('http://localhost:5000/users')
+      return {users:data}
+    }
+  }
+
+</script>
+
+<style>
+</style>
---
+++ newFront/myyoutubeapp/pages/test.vue
@@ -0,0 +1,7 @@
+<template>
+  <article>
+    <section>
+      <h1>This is a testing page</h1>
+    </section>
+  </article>
+</template>
