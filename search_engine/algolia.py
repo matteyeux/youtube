5264c462a93abd4424fc5169e47327c4b39663ef
mathieu <matteyeux@0day.cool>
2019-06-26 22:10:11 +0200

    update algolia

--- search_engine/algolia.py
+++ search_engine/algolia.py
@@ -3,30 +3,22 @@ import json
 import configparser
 import requests
 
-ini = configparser.ConfigParser()
-ini.read('config.ini')
-app_id = ini['YOUTUBE']['app_id']
-admin_key = ini['YOUTUBE']['admin_key']
+def send_data_to_algolia():
+	ini = configparser.ConfigParser()
+	ini.read('config.ini')
+	app_id = ini['YOUTUBE']['app_id']
+	admin_key = ini['YOUTUBE']['admin_key']
 
-client = SearchClient.create(app_id, admin_key)
-index = client.init_index('videos')
+	client = SearchClient.create(app_id, admin_key)
+	index = client.init_index('videos')
 
-URL = "http://127.0.0.1:5000/videos"
-r = requests.get(URL)
+	URL = "http://127.0.0.1:5000/videos"
+	r = requests.get(URL)
 
-data = r.json()
-print(data['data'])
+	data = r.json()
 
-# send data to algolia
-index.save_objects(data['data'], {'autoGenerateObjectIDIfNotExist': True})
+	# send data to algolia
+	index.save_objects(data['data'], {'autoGenerateObjectIDIfNotExist': True})
 
-# # Search for a first name
-# print(json.dumps(index.search('matteyeux'), indent=4))
-# Search for a first name with typo
-#print(index.search('jimie'))
-## Search for a company
-#print(index.search('Alex'))
-## Search for a first name and a company
-#print(index.search('jimmie paint'))
-
-
+if __name__ == '__main__':
+	send_data_to_algolia()
